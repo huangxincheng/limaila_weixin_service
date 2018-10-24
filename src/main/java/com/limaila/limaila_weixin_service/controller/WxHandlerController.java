c1043c935e6f71b7458e8a0ba87048cb361356a5
黄信程 <huangxincheng@xmles.cn>
2018-10-24 18:37:34 +0800

    commit

--- src/main/java/com/limaila/limaila_weixin_service/controller/WxHandlerController.java
+++ src/main/java/com/limaila/limaila_weixin_service/controller/WxHandlerController.java
@@ -1,13 +1,56 @@
 package com.limaila.limaila_weixin_service.controller;
 
-import org.springframework.web.bind.annotation.*;
+import com.limaila.limaila_weixin_service.helper.wxAppServer.WxAppServerHelper;
+import com.limaila.limaila_weixin_service.helper.wxAppServer.WxSignHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @RestController
 @RequestMapping("/wxHandler")
 public class WxHandlerController {
 
+    @Autowired
+    private WxAppServerHelper wxAppServerHelper;
 
+    private static final Logger logger = LoggerFactory.getLogger(WxHandlerController.class);
 
+    @GetMapping
+    public void handlerWeixin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");  //微信服务器POST消息时用的是UTF-8编码，在接收时也要用同样的编码，否则中文会乱码；
+        response.setCharacterEncoding("UTF-8"); //在响应消息（回复消息给用户）时，也将编码方式设置为UTF-8，原理同上；boolean isGet =
+        PrintWriter out = response.getWriter();
+        String signature = request.getParameter("signature"); // 微信加密签名
+        String timestamp = request.getParameter("timestamp"); // 时间戳
+        String nonce = request.getParameter("nonce");         // 随机数
+        String echostr = request.getParameter("echostr");     // 随机字符串
+        String wxKey = request.getParameter("wxKey");         // 区分那个公众号标识
+        logger.info("============== 微信统一处理接口 ===============");
+        logger.info(" signature = " + signature);
+        logger.info(" timestamp = " + timestamp);
+        logger.info(" nonce = " + nonce);
+        logger.info(" echostr = " + echostr);
+        logger.info(" wxKey = " + wxKey);
+        logger.info("============== 微信统一处理接口 ===============");
+        try {
+            if (wxAppServerHelper.getWxAppServer(wxKey) == null) {
+                throw new RuntimeException("请求非法");
+            }
+            boolean check = WxSignHelper.checkSignature(wxAppServerHelper.getWxAppServer(wxKey).getAppToken(), signature, timestamp, nonce);
+            if (check) {
+                // 注意此处必须返回echostr以完成验证
+                out.write(echostr);
+            }
+        } finally {
+            out.close();
+        }
+    }
 }
--- src/main/java/com/limaila/limaila_weixin_service/helper/wxAppServer/WxSignHelper.java
+++ src/main/java/com/limaila/limaila_weixin_service/helper/wxAppServer/WxSignHelper.java
@@ -1,8 +1,34 @@
 package com.limaila.limaila_weixin_service.helper.wxAppServer;
 
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
 import java.util.Arrays;
 
 public class WxSignHelper {
 
+    private static final Logger logger = LoggerFactory.getLogger(WxSignHelper.class);
+    /**
+     * 开发者通过检验signature对请求进行校验
+     * @param token token
+     * @param signature signature
+     * @param timestamp timestamp
+     * @param nonce nonce
+     * @return
+     */
+    public static boolean checkSignature(String token, String signature, String timestamp, String nonce) {
+        String[] arr = new String[]{token, timestamp, nonce};
+//        1）将token、timestamp、nonce三个参数进行字典序排序
+        Arrays.sort(arr);
+//        2）将三个参数字符串拼接成一个字符串进行sha1加密
+        String tmpStr = DigestUtils.sha1Hex(arr[0] + arr[1] + arr[2]);
+//        3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+        boolean check = StringUtils.pathEquals(signature, tmpStr);
+        logger.info("{} {} {}", WxSignHelper.class.getSimpleName(), "checkSignature", check);
+        return check;
+    }
 
 }
