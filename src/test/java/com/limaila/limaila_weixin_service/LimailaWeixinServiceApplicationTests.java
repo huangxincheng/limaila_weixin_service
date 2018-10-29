ab5b9cef8c5356846d2a30584f3be9203a6a98d3
黄信程 <huangxincheng@xmles.cn>
2018-10-29 19:09:39 +0800

    增加代码测试

--- src/test/java/com/limaila/limaila_weixin_service/LimailaWeixinServiceApplicationTests.java
+++ src/test/java/com/limaila/limaila_weixin_service/LimailaWeixinServiceApplicationTests.java
@@ -4,9 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,11 +23,22 @@ public class LimailaWeixinServiceApplicationTests {
         vo.set("a1","a2");
         String a1 = vo.get("a1");
         System.out.println(a1);
+
+        HashMap map = new HashMap<>();
+        map.put("a6", "a8");
+        reidsCacheTemplate.opsForValue().set("bbq", map);
+
+        HashMap mp = (HashMap)reidsCacheTemplate.opsForValue().get("bbq");
+        System.out.println(mp);
     }
 
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+
+    @Autowired
+    private RedisTemplate<String, Serializable> reidsCacheTemplate;
 
 
 }
