ba13479df1e7d514f2405cafbe67458ad82e85a2
黄信程 <huangxincheng@xmles.cn>
2018-10-29 18:47:02 +0800

    增加缓存

--- src/test/java/com/limaila/limaila_weixin_service/LimailaWeixinServiceApplicationTests.java
+++ src/test/java/com/limaila/limaila_weixin_service/LimailaWeixinServiceApplicationTests.java
@@ -2,7 +2,10 @@ package com.limaila.limaila_weixin_service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +14,15 @@ public class LimailaWeixinServiceApplicationTests {
 
     @Test
     public void contextLoads() {
+        ValueOperations<String, String> vo = redisTemplate.opsForValue();
+        vo.set("a1","a2");
+        String a1 = vo.get("a1");
+        System.out.println(a1);
     }
+
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
 
 }
