73b6bd6e3891906b09a4461a101bb2c4dbe04d6c
黄信程 <huangxincheng@xmles.cn>
2018-10-30 19:10:12 +0800

    * restTemplate作为spring web client下的一个工具类 对http请求做了一层封装，用起来也更加简洁容易，          * 但最近遇到一个问题就是在发送请求时由于请求中包含中文导致乱码，都变成???????一堆问号，网上很多解决方案，但很多都比较…..          * 可以看到StringHttpMessageConverter默认是ISO-8859-1编码（有空可以看下别的MessageConverter其实都是UTF-8的），所以就把它改了就好啦~          * 先看说如何解决：

--- src/main/java/com/limaila/limaila_weixin_service/LimailaWeixinServiceApplication.java
+++ src/main/java/com/limaila/limaila_weixin_service/LimailaWeixinServiceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class LimailaWeixinServiceApplication {
@@ -19,6 +22,14 @@ public class LimailaWeixinServiceApplication {
     public RestTemplate restTemplate(ClientHttpRequestFactory factory){
         RestTemplate restTemplate = new RestTemplate(factory);
 //        restTemplate.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
+        /**
+         * restTemplate作为spring web client下的一个工具类 对http请求做了一层封装，用起来也更加简洁容易，
+         * 但最近遇到一个问题就是在发送请求时由于请求中包含中文导致乱码，都变成???????一堆问号，网上很多解决方案，但很多都比较…..
+         * 可以看到StringHttpMessageConverter默认是ISO-8859-1编码（有空可以看下别的MessageConverter其实都是UTF-8的），所以就把它改了就好啦~
+         * 先看说如何解决：
+         * restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+         */
+        restTemplate.getMessageConverters().add(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
         return restTemplate;
     }
 
